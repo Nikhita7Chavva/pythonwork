faa68e9df5ef9a3df046fa145b99d128d2f1815d
Nikhita7Chavva <54998946+Nikhita7Chavva@users.noreply.github.com>
2020-04-07 20:27:33 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,66 +1,13 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 12,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "enter the number5\n",
-      "0\n",
-      "1\n",
-      "1\n",
-      "2\n",
-      "3\n"
-     ]
-    }
-   ],
-   "source": [
-    "a=0\n",
-    "b=1\n",
-    "c=a+b\n",
-    "n=int(input(\"enter the number\"))\n",
-    "\n",
-    "print(a)\n",
-    "print(b)\n",
-    "for i in range(2,n):\n",
-    "    print(c)\n",
-    "    a=b\n",
-    "    b=c\n",
-    "    c=a+b\n",
-    "\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+a=0
+b=1
+c=a+b
+n=int(input("enter the number"))
+
+print(a)
+print(b)
+for i in range(2,n):
+    print(c)
+    a=b
+    b=c
+    c=a+b
+
